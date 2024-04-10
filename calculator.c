d99462bb7382d4d9758895b4bdb205ea6ef8dca9
Acrotwit <118976236+Acrotwit@users.noreply.github.com>
2024-04-10 11:46:42 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,1 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    char function = get_char("what function do you want to use? A for Addition or D for Division");
+    long x = get_long("x: ");
+    long y = get_long("y: ");
+
+    float z = (float) x / (float) y;
+if (function == 'A')
+    {
+      printf("%li\n", x + y);
+    }
+    else if (function == 'D')
+    printf("%f\n", z);
+}
